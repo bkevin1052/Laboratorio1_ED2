4e82d113a338264f57bc8a08fb2c47cc982caf06
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-08-24 17:01:21 -0600

    agregando recyclerView canciones

--- app/src/main/java/com/laboratoriorepaso/kevin/laboratoriorepaso/PrincipalActivity.java
+++ app/src/main/java/com/laboratoriorepaso/kevin/laboratoriorepaso/PrincipalActivity.java
@@ -1,10 +1,12 @@
 package com.laboratoriorepaso.kevin.laboratoriorepaso;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.laboratoriorepaso.adaptador.AdapterCancion;
 import com.laboratoriorepaso.clases.Cancion;
@@ -16,12 +18,17 @@ public class PrincipalActivity extends AppCompatActivity {
     RecyclerView recyclerViewCancion;
     AdapterCancion adapterCancion;
     Button playList;
+
+    //declaracion de hash table
     HashMap<String,Cancion> listaCanciones = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        //Indicando ID al objeto
+        playList = (Button)findViewById(R.id.verPlaylist);
+        recyclerViewCancion = (RecyclerView) findViewById(R.id.RecyclerViewVerTodas);
         //Creacion de canciones
         listaCanciones.put("Es Por Ti",new Cancion("Es Por Ti","Pop Latino","Pa Dentro",500,R.drawable.cancion1,"Juanes"));
         listaCanciones.put("Waiting For Love",new Cancion("Waiting For Love","Electronica","Stories",300,R.drawable.cancion2,"Avicii"));
@@ -35,12 +42,22 @@ public class PrincipalActivity extends AppCompatActivity {
         listaCanciones.put("Flojos de pantalón",new Cancion("Flojos de pantalón","Rock","Jugar al gua",500,R.drawable.cancion10,"Rosendo"));
         listaCanciones.put("A quién le importa",new Cancion("A quién le importa","Electronica","Operación Vodevil",500,R.drawable.cancion11,"Fangoria"));
 
-
-
-        recyclerViewCancion = (RecyclerView) findViewById(R.id.RecyclerViewVerTodas);
+        //configuracion del adapter y recyclerview
         recyclerViewCancion.setLayoutManager(new LinearLayoutManager(this));
         adapterCancion = new AdapterCancion(this, listaCanciones);
         recyclerViewCancion.setAdapter(adapterCancion);
+
+        adapterCancion.setOnClickListener(view -> {
+            PlayListActivity.playList.add(listaCanciones.get(recyclerViewCancion.getChildAdapterPosition(view)));
+            Toast.makeText(getApplicationContext(),"Canciones agregada a Play List",Toast.LENGTH_SHORT).show();
+        });
+
+
+        //Boton ver Play list
+        playList.setOnClickListener(view->{
+            startActivity(new Intent(PrincipalActivity.this,PlayListActivity.class));
+        });
+
 
     }
 }
