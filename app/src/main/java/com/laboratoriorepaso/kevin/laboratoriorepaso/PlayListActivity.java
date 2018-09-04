eb9a018f14018fe828ff5787c5f78ef28c196b99
Kevin Estuardo Barrientos Calderon <bkevin1052@gmail.com>
2018-09-04 17:51:54 -0600

    Agregando orden ASCENDENTE/DESCENDENTE en playlist

--- app/src/main/java/com/laboratoriorepaso/kevin/laboratoriorepaso/PlayListActivity.java
+++ app/src/main/java/com/laboratoriorepaso/kevin/laboratoriorepaso/PlayListActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,14 +14,18 @@ import com.laboratoriorepaso.adaptador.AdapterPlayList;
 import com.laboratoriorepaso.clases.Cancion;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class PlayListActivity extends AppCompatActivity {
 
     RecyclerView recyclerViewCancion;
     AdapterPlayList adapterPlayList;
-
     TextView eliminar;
+
+    Button btn1, btn2,btn3,btn4;
     public static List<Cancion> playList= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,10 @@ public class PlayListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play_list);
 
         eliminar = (TextView)findViewById(R.id.eliminar);
+        btn1 = (Button)findViewById(R.id.DescendentePorDuracion);
+        btn2 = (Button)findViewById(R.id.AscendentePorDuracion);
+        btn3 = (Button)findViewById(R.id.DescendentePorNombre);
+        btn4 = (Button)findViewById(R.id.AscendentePorNombre);
 
         recyclerViewCancion = (RecyclerView) findViewById(R.id.RecyclerViewPlayList);
         recyclerViewCancion.setLayoutManager(new LinearLayoutManager(this));
@@ -52,6 +61,27 @@ public class PlayListActivity extends AppCompatActivity {
         adapterPlayList.setOnClickListener(view -> {
             Toast.makeText(getApplicationContext(),"Reproduciendo cancion",Toast.LENGTH_SHORT).show();
         });
+
+        btn1.setOnClickListener(view ->{
+            Collections.sort(playList, (o1, o2) -> o2.getDuracion().compareToIgnoreCase(o1.getDuracion()));
+            adapterPlayList.eliminar(playList);
+        });
+
+        btn2.setOnClickListener(view ->{
+            Collections.sort(playList, (o1, o2) -> o1.getDuracion().compareToIgnoreCase(o2.getDuracion()));
+            adapterPlayList.eliminar(playList);
+        });
+
+        btn3.setOnClickListener(view ->{
+            Collections.sort(playList, (o1, o2) -> o2.getNombre().compareToIgnoreCase(o1.getNombre()));
+            adapterPlayList.eliminar(playList);
+        });
+
+        btn4.setOnClickListener(view ->{
+            Collections.sort(playList, (o1, o2) -> o1.getNombre().compareToIgnoreCase(o2.getNombre()));
+            adapterPlayList.eliminar(playList);
+        });
+
 
     }
 }
